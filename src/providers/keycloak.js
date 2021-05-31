a23bd3393a4efb1319c576903f346343c370321f
SyedAkash <mahmud.hasan@dsinnovators.com>
2021-05-31 15:21:35 +0600

    Keycloak Auth added

--- src/providers/keycloak.js
+++ src/providers/keycloak.js
@@ -1,12 +1,5 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
-
 const options = {
     providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
         {
             id: "keycloak",
             name: "Keycloak",
@@ -17,13 +10,10 @@ const options = {
             accessTokenUrl: `${process.env.KEYCLOAK_BASE_URL}/token`,
             requestTokenUrl: `${process.env.KEYCLOAK_BASE_URL}/auth`,
             authorizationUrl: `${process.env.KEYCLOAK_BASE_URL}/auth?response_type=code`,
-            clientId: "next-client",
+            clientId: "nextjs_local",
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
             profileUrl: `${process.env.KEYCLOAK_BASE_URL}/userinfo`,
             profile: (profile) => ({ ...profile, id: profile.sub }),
-        }
+        },
     ],
-
-}
-
-export default (req, res) => NextAuth(req, res, options)
+};
